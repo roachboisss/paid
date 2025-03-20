02437f2960a80b559269a182709efd587556a95c
Phillip <108208254+roachboisss@users.noreply.github.com>
2025-03-20 12:56:19 -0400

    Update info.js

--- info.js
+++ info.js
@@ -1,7 +1,7 @@
 const businessInfo = {
   "title": "Title of Page",
   "businessname": "Name of the business",
-  "heading": "The big text on the front page",
+  "heading": "NIGGER",
   "heatingdescription": "The description of the heading",
   "ourservicesdescription": "Description of the Our Services title",
   "service1": "Service 1",
